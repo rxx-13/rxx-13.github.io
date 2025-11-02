e1ff38046a038124445bed2684a7d5e17f68ff55
rxx-13 <dmitryaladd@gmail.com>
2025-11-02 09:07:01 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToGoogle() {
+    window.open('https://www.google.com', '_blank');
+}
+
+function goToYandex() {
+    window.open('https://yandex.ru', '_blank');
+}
+
+function goToDuckDuckGo() {
+    window.open('https://duckduckgo.com', '_blank');
+}
